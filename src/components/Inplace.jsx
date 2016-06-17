151afda0965591c2bcaca472c44669da58aea99e
Dominik Burgdörfer <db@domachine.de>
2016-06-17 14:50:53 +0200

    Tweak inplace component

--- src/components/Inplace.jsx
+++ src/components/Inplace.jsx
@@ -1,27 +1,27 @@
-import ReactDOM from 'react-dom';
-import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
 import { connect } from '../lib/inplace-react'
 
 class Inplace extends Component {
-  constructor() {
-    super();
-    this.onChange = this.onChange.bind(this);
+  constructor () {
+    super()
+    this.onChange = this.onChange.bind(this)
   }
 
-  shouldComponentUpdate(nextProps) {
-    return nextProps.value !== ReactDOM.findDOMNode(this).innerHTML;
+  shouldComponentUpdate (nextProps) {
+    return nextProps.value !== ReactDOM.findDOMNode(this).textContent
   }
 
-  onChange() {
-    const html = ReactDOM.findDOMNode(this).innerHTML;
+  onChange () {
+    const html = ReactDOM.findDOMNode(this).textContent
     if (html !== this.lastHtml) {
       this.props.onUpdate({ [this.props.name]: html })
     }
-    this.lastHtml = html;
+    this.lastHtml = html
   }
 
-  render() {
-    const tag = this.props.tag || 'div';
+  render () {
+    const tag = this.props.tag || 'div'
     const { content: { [this.props.name]: value } } = this.props
     if (this.props.isEditable) {
       return React.createElement(tag, {
@@ -29,12 +29,12 @@ class Inplace extends Component {
         onBlur: this.onChange,
         contentEditable: true,
         className: this.props.className,
-        dangerouslySetInnerHTML: { __html: value },
+        children: value
       })
     } else {
       return React.createElement(tag, {
         className: this.props.className,
-        dangerouslySetInnerHTML: { __html: value },
+        children: value
       })
     }
   }
