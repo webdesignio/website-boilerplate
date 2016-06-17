bdaa4f881998392e7bc22e0c91fd542fe35eb50e
Dominik Burgdörfer <db@domachine.de>
2016-06-17 14:33:50 +0200

    Change builtin transforms api

--- scripts/builtin_transforms.js
+++ scripts/builtin_transforms.js
@@ -2,13 +2,19 @@
 
 module.exports = {
   react: {
-    imports: [
-      ['React', 'react'],
-      ['ReactDOM', 'react-dom']
-    ],
+    imports: {
+      dom: [
+        ['React', 'react'],
+        ['ReactDOM', 'react-dom']
+      ],
+      string: [
+        ['React', 'react'],
+        ['ReactDOM', 'react-dom/server']
+      ]
+    },
     dom: C => (props, el) => ReactDOM.render(React.createElement(C, props), el),
     string: C =>
       props =>
-        ReactDOM.renderToStaticMarkup(React.createElement($component_name, props))
+        ReactDOM.renderToStaticMarkup(React.createElement(C, props))
   }
 }
