ef90e6426064025fd3198b1abfc9d60c03c558d4
Dominik Burgdörfer <db@domachine.de>
2016-06-20 10:48:31 +0200

    Add mongodb; adapt rendering mechanism
    This closes #1.

--- server.js
+++ server.js
@@ -3,7 +3,12 @@
 const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
-const { Page, Object: _Object } = require('webdesignio')
+const { Page, Object: _Object, renderTemplate } = require('webdesignio')
+const mongoose = require('mongoose')
+const error = require('http-errors')
+
+mongoose.Promise = Promise
+mongoose.connect('mongodb://localhost/webdesignio')
 
 const app = express()
 app.set('view engine', 'pug')
@@ -13,25 +18,48 @@ app.get('/static/client.js', (req, res) => {
   res.sendFile(`${process.cwd()}/client.js`)
 })
 
-app.get('/:type/:id', (req, res, next) => {
-  res.render(`objects/${req.params.type}`)
+app.get('/:type/new', (req, res, next) => {
+  const o = new _Object({ data: {} })
+  res.render(`objects/${req.params.type}`, (err, html) => {
+    if (err) return next(err)
+    res.send(renderTemplate(o.data, html))
+  })
 })
 
-app.get('/:page', (req, res, next) => {
-  res.render(`pages/${req.params.page}`)
+app.get('/:type/:object', (req, res, next) => {
+  const { params } = req
+  _Object.findById(params.object)
+    .then(object => object == null ? Promise.reject(error(404)) : object)
+    .then(object =>
+      new Promise((resolve, reject) => {
+        res.render(`objects/${req.params.type}`, (err, html) => {
+          if (err) return reject(err)
+          resolve(renderTemplate(object.data, html))
+        })
+      })
+    )
+    .then(html => res.send(html))
+    .catch(next)
 })
 
-app.get('/api/v1/pages/:page', (req, res) =>
-  res.send(new Page({
-    _id: req.params.page, data: { title: req.params.page }
-  }))
-)
-
-app.get('/api/v1/objects/:object', (req, res) =>
-  res.send(new _Object({
-    _id: req.params.object, data: { title: req.params.object }
-  }))
-)
+app.get(/\/([^/]*)/, (req, res, next) => {
+  const { params } = req
+  const pageID = params[0] || 'index'
+  const createPage = () =>
+    new Page({ _id: pageID, data: { title: pageID } })
+  Page.findById(pageID)
+    .then(page => page == null ? createPage() : page)
+    .then(page =>
+      new Promise((resolve, reject) => {
+        res.render(`pages/${pageID}`, (err, html) => {
+          if (err) return reject(err)
+          resolve(renderTemplate(page.data, html))
+        })
+      })
+    )
+    .then(html => res.send(html))
+    .catch(next)
+})
 
 const srv = http.createServer(app)
 srv.listen(process.env.PORT || 3000, () => {
--- src/client.js
+++ src/client.js
@@ -1,18 +1,7 @@
-/* global location, fetch */
-
-import { parse } from 'url'
 import { findAndRender } from 'webdesignio'
-import 'whatwg-fetch'
 
 import components from './components'
 
-const { pathname } = parse(location.href)
-const parts = pathname.split('/').filter(p => !!p)
-const contentPath = parts.length === 1
-  ? `/api/v1/pages/${parts[0]}`
-  : `/api/v1/objects/${parts[1]}`
-fetch(contentPath)
-  .then(res => res.json())
-  .then(({ data }) => {
-    findAndRender(components, { initialContent: data })
-  })
+export default function bootstrap (initialContent) {
+  findAndRender(components, { initialContent })
+}
