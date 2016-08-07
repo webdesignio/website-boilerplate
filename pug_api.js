e51a87acbea70a5c3da2fdde234598d9fccb0f79
Dominik Burgdörfer <db@domachine.de>
2016-08-07 11:30:16 +0200

    Resolve asset paths from domain

--- pug_api.js
+++ pug_api.js
@@ -1,7 +1,16 @@
+const { render } = require('mustache')
+
 module.exports = {
   asset (path) {
     if (process.env.NODE_ENV === 'production') {
-      return 'http://localhost:8080' + path
+      const config = require('./.webdesigniorc.json').config || {}
+      const settings = require('./package.json').webdesignio || {}
+      const domainTemplate = config.domain || ''
+      const protocol = config.protocol || 'https'
+      const { defaultLanguage } = settings
+      if (!defaultLanguage) throw new Error('No default language given!')
+      const domain = render(domainTemplate, { language: defaultLanguage })
+      return `${protocol}://` + domain.replace(/\/+$/, '') + path
     }
     return path
   }
